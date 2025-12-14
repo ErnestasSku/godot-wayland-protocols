606b506173f57a3c1fbe2e4dc313fc45d2e3414e
ernestassku <ernestassku@gmail.com>
2025-12-14 13:02:15 +0200

    Implement polling and fix incorrect event dispatching

--- include/workspace.h
+++ include/workspace.h
@@ -18,66 +18,56 @@ class Workspace : public RefCounted {
 
 private:
   // ext_workspace_manager
-  static void workspace_group(void *, ext_workspace_manager_v1 *,
-                              ext_workspace_group_handle_v1 *);
-  static void workspace(void *, ext_workspace_manager_v1 *,
-                        ext_workspace_handle_v1 *);
+  static void workspace_group(void *, ext_workspace_manager_v1 *, ext_workspace_group_handle_v1 *);
+  static void workspace(void *, ext_workspace_manager_v1 *, ext_workspace_handle_v1 *);
   static void workspace_done(void *, ext_workspace_manager_v1 *);
   static void workspace_finished(void *, ext_workspace_manager_v1 *);
 
   // ext_workspace_group_hanlder
-  static void workspace_group_capabilities(void *,
-                                           ext_workspace_group_handle_v1 *,
-                                           uint32_t);
-  static void
-  workspace_group_output_enter(void *, struct ext_workspace_group_handle_v1 *,
-                               struct wl_output *);
+  static void workspace_group_capabilities(void *, ext_workspace_group_handle_v1 *, uint32_t);
+  static void workspace_group_output_enter(void *, struct ext_workspace_group_handle_v1 *, struct wl_output *);
 
-  static void
-  workspace_group_output_leave(void *, struct ext_workspace_group_handle_v1 *,
-                               struct wl_output *);
-  static void
-  workspace_group_workspace_enter(void *,
-                                  struct ext_workspace_group_handle_v1 *,
-                                  struct ext_workspace_handle_v1 *);
+  static void workspace_group_output_leave(void *, struct ext_workspace_group_handle_v1 *, struct wl_output *);
+  static void workspace_group_workspace_enter(void *, struct ext_workspace_group_handle_v1 *,
+                                              struct ext_workspace_handle_v1 *);
 
-  static void
-  workspace_group_workspace_leave(void *data,
-                                  struct ext_workspace_group_handle_v1 *,
-                                  struct ext_workspace_handle_v1 *);
-  static void workspace_group_remove(void *data,
-                                     struct ext_workspace_group_handle_v1 *);
+  static void workspace_group_workspace_leave(void *data, struct ext_workspace_group_handle_v1 *,
+                                              struct ext_workspace_handle_v1 *);
+  static void workspace_group_remove(void *data, struct ext_workspace_group_handle_v1 *);
 
   // ext_workspace_handle
-  static void workspace_id(void *, struct ext_workspace_handle_v1 *,
-                           const char *);
+  static void workspace_id(void *, struct ext_workspace_handle_v1 *, const char *);
 
-  static void workspace_name(void *, struct ext_workspace_handle_v1 *,
-                             const char *);
+  static void workspace_name(void *, struct ext_workspace_handle_v1 *, const char *);
 
-  static void workspace_coordinates(void *, struct ext_workspace_handle_v1 *,
-                                    struct wl_array *);
+  static void workspace_coordinates(void *, struct ext_workspace_handle_v1 *, struct wl_array *);
 
-  static void workspace_state(void *data, struct ext_workspace_handle_v1 *,
-                              uint32_t state);
+  static void workspace_state(void *data, struct ext_workspace_handle_v1 *, uint32_t state);
 
-  static void workspace_capabilities(void *data,
-                                     struct ext_workspace_handle_v1 *,
-                                     uint32_t);
+  static void workspace_capabilities(void *data, struct ext_workspace_handle_v1 *, uint32_t);
   static void workspace_removed(void *, struct ext_workspace_handle_v1 *);
+
+  ext_workspace_manager_v1_listener manager_listener;
+  bool manager_listener_installed = false;
 
   class WorkspaceGroupWrapper {
   public:
-    WorkspaceGroupWrapper(ext_workspace_group_handle_v1 *handle) {
-      this->handle = handle;
+    // WorkspaceGroupWrapper(ext_workspace_group_handle_v1 *handle) { this->handle = handle; }
+    WorkspaceGroupWrapper(ext_workspace_group_handle_v1 *handle) : handle(handle) {
+      workspace_group_listener =
+          new ext_workspace_group_handle_v1_listener{.capabilities = workspace_group_capabilities,
+                                                     .output_enter = workspace_group_output_enter,
+                                                     .output_leave = workspace_group_output_leave,
+                                                     .workspace_enter = workspace_group_workspace_enter,
+                                                     .workspace_leave = workspace_group_workspace_leave,
+                                                     .removed = workspace_group_remove};
     }
-
     ~WorkspaceGroupWrapper() {
       if (handle) {
         UtilityFunctions::print("DEBUG: workspace group destructor was called");
         auto wayland = Wayland::get_singleton();
         ext_workspace_group_handle_v1_destroy(handle);
-        wl_display_roundtrip(wayland->display);
+        // wl_display_roundtrip(wayland->display);
       }
     }
 
@@ -85,16 +75,25 @@ private:
     ext_workspace_group_handle_v1_group_capabilities capabilities;
     std::vector<ext_workspace_handle_v1 *> workspaces;
     std::vector<wl_output *> outputs;
+    ext_workspace_group_handle_v1_listener *workspace_group_listener;
   };
 
   class WorkspaceWrapper {
   public:
-    WorkspaceWrapper(ext_workspace_handle_v1 *handle) { this->handle = handle; }
+    WorkspaceWrapper(ext_workspace_handle_v1 *handle) : handle(handle) {
+      workspace_listener = new ext_workspace_handle_v1_listener{.id = Workspace::workspace_id,
+                                                                .name = Workspace::workspace_name,
+                                                                .coordinates = Workspace::workspace_coordinates,
+                                                                .state = Workspace::workspace_state,
+                                                                .capabilities = Workspace::workspace_capabilities,
+                                                                .removed = Workspace::workspace_removed};
+    }
+
     ~WorkspaceWrapper() {
       if (handle) {
         UtilityFunctions::print("DEBUG: workspace group destructor was called");
         ext_workspace_handle_v1_destroy(handle);
-        wl_display_roundtrip(Wayland::get_singleton()->display);
+        // wl_display_roundtrip(Wayland::get_singleton()->display);
       }
     }
 
@@ -104,14 +103,15 @@ private:
     wl_array *coordinates;
     ext_workspace_handle_v1_state state;
     ext_workspace_handle_v1_workspace_capabilities capabilities;
+    ext_workspace_handle_v1_listener *workspace_listener;
   };
 
   std::vector<std::unique_ptr<WorkspaceGroupWrapper>> groups;
   std::vector<std::unique_ptr<WorkspaceWrapper>> workspaces;
 
-  WorkspaceGroupWrapper* find_workspace_group_wrapper(ext_workspace_group_handle_v1*);
-  WorkspaceWrapper* find_workspace_wrapper(ext_workspace_handle_v1*);
-  
+  WorkspaceGroupWrapper *find_workspace_group_wrapper(ext_workspace_group_handle_v1 *);
+  WorkspaceWrapper *find_workspace_wrapper(ext_workspace_handle_v1 *);
+
 protected:
   static void _bind_methods();
 
@@ -119,8 +119,9 @@ public:
   Workspace();
   void register_to_workspace_events();
   void unregister_from_workspace_events();
-  void hello_workspace();
-  void test_increment();
+  void poll_events();
+  // void hello_workspace();
+  // void test_increment();
 };
 
 #endif
--- src/workspace.cpp
+++ src/workspace.cpp
@@ -1,14 +1,14 @@
 #include "workspace.h"
 #include "ext-workspace-v1.h"
-#include "godot_cpp/classes/global_constants.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/object.hpp"
-#include "godot_cpp/core/property_info.hpp"
 #include "godot_cpp/variant/utility_functions.hpp"
 #include "wayland.h"
 #include <algorithm>
 #include <cstdint>
 #include <memory>
+#include <poll.h>
+#include <sys/poll.h>
 #include <wayland-client-core.h>
 #include <wayland-client-protocol.h>
 
@@ -23,8 +23,8 @@ Workspace::Workspace() {
 }
 
 void Workspace::_bind_methods() {
-  ClassDB::bind_method(D_METHOD("register_to_workspace_events"),
-                       &Workspace::register_to_workspace_events);
+  ClassDB::bind_method(D_METHOD("register_to_workspace_events"), &Workspace::register_to_workspace_events);
+  ClassDB::bind_method(D_METHOD("poll_events"), &Workspace::poll_events);
 
   ADD_SIGNAL(MethodInfo("workspace_created"));
 }
@@ -33,43 +33,25 @@ void Workspace::workspace_group(void *data, ext_workspace_manager_v1 *manager,
                                 ext_workspace_group_handle_v1 *group_handle) {
 
   auto self = static_cast<Workspace *>(data);
-
-  auto workspace_group =
-      std::make_unique<Workspace::WorkspaceGroupWrapper>(group_handle);
-
+  auto workspace_group = std::make_unique<Workspace::WorkspaceGroupWrapper>(group_handle);
+  auto *group_ptr = workspace_group.get();
   self->groups.push_back(std::move(workspace_group));
 
-  ext_workspace_group_handle_v1_listener group_listener = {
-      .capabilities = workspace_group_capabilities,
-      .output_enter = workspace_group_output_enter,
-      .output_leave = workspace_group_output_leave,
-      .workspace_enter = workspace_group_workspace_enter,
-      .workspace_leave = workspace_group_workspace_leave,
-      .removed = workspace_group_remove};
-
-  ext_workspace_group_handle_v1_add_listener(group_handle, &group_listener,
-                                             data);
-  wl_display_roundtrip(Wayland::get_singleton()->display);
+  ext_workspace_group_handle_v1_add_listener(group_handle, group_ptr->workspace_group_listener, data);
+  // wl_display_roundtrip(Wayland::get_singleton()->display);
 };
 
-void Workspace::workspace(void *data, ext_workspace_manager_v1 *manager,
-                          ext_workspace_handle_v1 *workspace_handle) {
+void Workspace::workspace(void *data, ext_workspace_manager_v1 *manager, ext_workspace_handle_v1 *workspace_handle) {
 
   auto *self = static_cast<Workspace *>(data);
   auto workspace = std::make_unique<WorkspaceWrapper>(workspace_handle);
+  auto *workspace_ptr = workspace.get();
   self->workspaces.push_back(std::move(workspace));
 
-  ext_workspace_handle_v1_listener workspace_listener{
-      .id = workspace_id,
-      .name = workspace_name,
-      .coordinates = workspace_coordinates,
-      .state = workspace_state,
-      .capabilities = workspace_capabilities,
-      .removed = workspace_removed};
-
-  ext_workspace_handle_v1_add_listener(workspace_handle, &workspace_listener,
-                                       data);
-  wl_display_roundtrip(Wayland::get_singleton()->display);
+  ext_workspace_handle_v1_add_listener(workspace_handle, workspace_ptr->workspace_listener, data);
+  // wl_display_roundtrip(Wayland::get_singleton()->display);
+  UtilityFunctions::print("Workspace created I guess");
+  self->emit_signal("workspace_created");
 }
 
 void Workspace::workspace_done(void *data, ext_workspace_manager_v1 *manager) {
@@ -79,17 +61,16 @@ void Workspace::workspace_done(void *data, ext_workspace_manager_v1 *manager) {
 
   auto self = static_cast<Workspace *>(data);
 
-  UtilityFunctions::print("We have [groups]: ", self->groups.size());
-  UtilityFunctions::print("We have [workspaces]: ",
-                          (*self->groups[0]).workspaces.size());
-  UtilityFunctions::print("We have [wl_outputs]",
-                          (*self->groups[0]).outputs.size());
-  UtilityFunctions::print("We have [capabilities]",
-                          (*self->groups[0]).capabilities);
+  // UtilityFunctions::print("We have [groups]: ", self->groups.size());
+  // UtilityFunctions::print("We have [workspaces]: ",
+  //                         (*self->groups[0]).workspaces.size());
+  // UtilityFunctions::print("We have [wl_outputs]",
+  //                         (*self->groups[0]).outputs.size());
+  // UtilityFunctions::print("We have [capabilities]",
+  //                         (*self->groups[0]).capabilities);
 }
 
-void Workspace::workspace_finished(void *data,
-                                   ext_workspace_manager_v1 *manager) {
+void Workspace::workspace_finished(void *data, ext_workspace_manager_v1 *manager) {
   // TODO: This meanes no more events will be sent
   // some flag should be set, which would now allow to call any methods from
   // godot since after finished, it would probably crash, as the managed is
@@ -98,9 +79,8 @@ void Workspace::workspace_finished(void *data,
 }
 
 // Workspace group event listeners
-void Workspace::workspace_group_capabilities(
-    void *data, ext_workspace_group_handle_v1 *group_handle,
-    uint32_t capabilities) {
+void Workspace::workspace_group_capabilities(void *data, ext_workspace_group_handle_v1 *group_handle,
+                                             uint32_t capabilities) {
 
   auto self = static_cast<Workspace *>(data);
   // TODO: This will be repeated multiple times. So move it to a class method.
@@ -109,14 +89,11 @@ void Workspace::workspace_group_capabilities(
     return;
   }
 
-  (*workspace_group).capabilities =
-      static_cast<ext_workspace_group_handle_v1_group_capabilities>(
-          capabilities);
+  (*workspace_group).capabilities = static_cast<ext_workspace_group_handle_v1_group_capabilities>(capabilities);
 }
 
-void Workspace::workspace_group_output_enter(
-    void *data, struct ext_workspace_group_handle_v1 *group_handle,
-    struct wl_output *output) {
+void Workspace::workspace_group_output_enter(void *data, struct ext_workspace_group_handle_v1 *group_handle,
+                                             struct wl_output *output) {
 
   auto self = static_cast<Workspace *>(data);
   auto workspace_group = self->find_workspace_group_wrapper(group_handle);
@@ -128,9 +105,8 @@ void Workspace::workspace_group_output_enter(
   (*workspace_group).outputs.push_back(output);
 }
 
-void Workspace::workspace_group_output_leave(
-    void *data, struct ext_workspace_group_handle_v1 *group_handle,
-    struct wl_output *output) {
+void Workspace::workspace_group_output_leave(void *data, struct ext_workspace_group_handle_v1 *group_handle,
+                                             struct wl_output *output) {
   auto self = static_cast<Workspace *>(data);
   auto workspace_group = self->find_workspace_group_wrapper(group_handle);
 
@@ -139,14 +115,12 @@ void Workspace::workspace_group_output_leave(
   }
 
   (*workspace_group)
-      .outputs.erase(std::find_if(
-          (*workspace_group).outputs.begin(), (*workspace_group).outputs.end(),
-          [output](const wl_output *elem) { return elem == output; }));
+      .outputs.erase(std::find_if((*workspace_group).outputs.begin(), (*workspace_group).outputs.end(),
+                                  [output](const wl_output *elem) { return elem == output; }));
 }
 
-void Workspace::workspace_group_workspace_enter(
-    void *data, struct ext_workspace_group_handle_v1 *group_handle,
-    struct ext_workspace_handle_v1 *workspace) {
+void Workspace::workspace_group_workspace_enter(void *data, struct ext_workspace_group_handle_v1 *group_handle,
+                                                struct ext_workspace_handle_v1 *workspace) {
 
   auto self = static_cast<Workspace *>(data);
   auto workspace_group = self->find_workspace_group_wrapper(group_handle);
@@ -157,9 +131,8 @@ void Workspace::workspace_group_workspace_enter(
   (*workspace_group).workspaces.push_back(workspace);
 }
 
-void Workspace::workspace_group_workspace_leave(
-    void *data, struct ext_workspace_group_handle_v1 *group_handle,
-    struct ext_workspace_handle_v1 *workspace) {
+void Workspace::workspace_group_workspace_leave(void *data, struct ext_workspace_group_handle_v1 *group_handle,
+                                                struct ext_workspace_handle_v1 *workspace) {
 
   auto self = static_cast<Workspace *>(data);
   auto workspace_group = self->find_workspace_group_wrapper(group_handle);
@@ -168,22 +141,15 @@ void Workspace::workspace_group_workspace_leave(
   }
 
   (*workspace_group)
-      .workspaces.erase(
-          std::find_if((*workspace_group).workspaces.begin(),
-                       (*workspace_group).workspaces.end(),
-                       [workspace](const ext_workspace_handle_v1 *elem) {
-                         return elem == workspace;
-                       }));
+      .workspaces.erase(std::find_if((*workspace_group).workspaces.begin(), (*workspace_group).workspaces.end(),
+                                     [workspace](const ext_workspace_handle_v1 *elem) { return elem == workspace; }));
 }
 
-void Workspace::workspace_group_remove(
-    void *data, struct ext_workspace_group_handle_v1 *group_handle) {
+void Workspace::workspace_group_remove(void *data, struct ext_workspace_group_handle_v1 *group_handle) {
   auto self = static_cast<Workspace *>(data);
   auto workspace_group = std::find_if(
       self->groups.begin(), self->groups.end(),
-      [group_handle](const std::unique_ptr<WorkspaceGroupWrapper> &elem) {
-        return elem->handle == group_handle;
-      });
+      [group_handle](const std::unique_ptr<WorkspaceGroupWrapper> &elem) { return elem->handle == group_handle; });
 
   if (workspace_group == self->groups.end()) {
     return;
@@ -192,10 +158,8 @@ void Workspace::workspace_group_remove(
   self->groups.erase(workspace_group);
 }
 
-// Workspace evebt listeners
-void Workspace::workspace_id(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-    const char *id) {
+// Workspace event listeners
+void Workspace::workspace_id(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1, const char *id) {
   auto self = static_cast<Workspace *>(data);
   auto workspace = self->find_workspace_wrapper(ext_workspace_handle_v1);
 
@@ -206,9 +170,7 @@ void Workspace::workspace_id(
   }
 }
 
-void Workspace::workspace_name(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-    const char *name) {
+void Workspace::workspace_name(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1, const char *name) {
   auto self = static_cast<Workspace *>(data);
   auto workspace = self->find_workspace_wrapper(ext_workspace_handle_v1);
   if (workspace != nullptr) {
@@ -216,9 +178,8 @@ void Workspace::workspace_name(
   }
 }
 
-void Workspace::workspace_coordinates(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-    struct wl_array *coordinates) {
+void Workspace::workspace_coordinates(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
+                                      struct wl_array *coordinates) {
   UtilityFunctions::print("Coords: ", coordinates);
   auto self = static_cast<Workspace *>(data);
   auto workspace = self->find_workspace_wrapper(ext_workspace_handle_v1);
@@ -228,9 +189,7 @@ void Workspace::workspace_coordinates(
   }
 }
 
-void Workspace::workspace_state(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-    uint32_t state) {
+void Workspace::workspace_state(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1, uint32_t state) {
 
   auto self = static_cast<Workspace *>(data);
   auto workspace = self->find_workspace_wrapper(ext_workspace_handle_v1);
@@ -240,39 +199,30 @@ void Workspace::workspace_state(
   }
 }
 
-void Workspace::workspace_capabilities(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
-    uint32_t capabilities) {
+void Workspace::workspace_capabilities(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1,
+                                       uint32_t capabilities) {
   auto self = static_cast<Workspace *>(data);
   auto workspace = self->find_workspace_wrapper(ext_workspace_handle_v1);
 
   if (workspace != nullptr) {
-    (*workspace).capabilities =
-        static_cast<ext_workspace_handle_v1_workspace_capabilities>(
-            capabilities);
+    (*workspace).capabilities = static_cast<ext_workspace_handle_v1_workspace_capabilities>(capabilities);
   }
 }
-void Workspace::workspace_removed(
-    void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1) {
+void Workspace::workspace_removed(void *data, struct ext_workspace_handle_v1 *ext_workspace_handle_v1) {
 
   auto self = static_cast<Workspace *>(data);
 
   self->workspaces.erase(
       std::find_if(self->workspaces.begin(), self->workspaces.end(),
-                   [ext_workspace_handle_v1](auto &elem) {
-                     return elem->handle == ext_workspace_handle_v1;
-                   }));
+                   [ext_workspace_handle_v1](auto &elem) { return elem->handle == ext_workspace_handle_v1; }));
 }
 
 // Utility methods
 
-Workspace::WorkspaceGroupWrapper *
-Workspace::find_workspace_group_wrapper(ext_workspace_group_handle_v1 *handle) {
-  auto workspace_group = std::find_if(
-      groups.begin(), groups.end(),
-      [handle](const std::unique_ptr<WorkspaceGroupWrapper> &elem) {
-        return elem->handle == handle;
-      });
+Workspace::WorkspaceGroupWrapper *Workspace::find_workspace_group_wrapper(ext_workspace_group_handle_v1 *handle) {
+  auto workspace_group =
+      std::find_if(groups.begin(), groups.end(),
+                   [handle](const std::unique_ptr<WorkspaceGroupWrapper> &elem) { return elem->handle == handle; });
 
   if (workspace_group == groups.end()) {
     return nullptr;
@@ -281,11 +231,9 @@ Workspace::find_workspace_group_wrapper(ext_workspace_group_handle_v1 *handle) {
   }
 }
 
-Workspace::WorkspaceWrapper *
-Workspace::find_workspace_wrapper(ext_workspace_handle_v1 *handle) {
+Workspace::WorkspaceWrapper *Workspace::find_workspace_wrapper(ext_workspace_handle_v1 *handle) {
   auto workspace =
-      std::find_if(workspaces.begin(), workspaces.end(),
-                   [handle](auto &elem) { return elem->handle == handle; });
+      std::find_if(workspaces.begin(), workspaces.end(), [handle](auto &elem) { return elem->handle == handle; });
   if (workspace == workspaces.end()) {
     return nullptr;
   } else {
@@ -296,14 +244,49 @@ Workspace::find_workspace_wrapper(ext_workspace_handle_v1 *handle) {
 // Godot methods
 void Workspace::register_to_workspace_events() {
   auto wayland = Wayland::get_singleton();
+  if (!wayland->workspace_manager || manager_listener_installed)
+    return;
 
-  const ext_workspace_manager_v1_listener workspace_manager_listener = {
-      .workspace_group = workspace_group,
-      .workspace = workspace,
-      .done = workspace_done,
-      .finished = workspace_finished};
+  manager_listener = {.workspace_group = workspace_group,
+                      .workspace = workspace,
+                      .done = workspace_done,
+                      .finished = workspace_finished};
 
-  ext_workspace_manager_v1_add_listener(wayland->workspace_manager,
-                                        &workspace_manager_listener, this);
-  wl_display_roundtrip(wayland->display);
+  ext_workspace_manager_v1_add_listener(wayland->workspace_manager, &manager_listener, this);
+
+  manager_listener_installed = true;
+  // auto wayland = Wayland::get_singleton();
+
+  // const ext_workspace_manager_v1_listener workspace_manager_listener = {.workspace_group = workspace_group,
+  //                                                                       .workspace = workspace,
+  //                                                                       .done = workspace_done,
+  //                                                                       .finished = workspace_finished};
+
+  // ext_workspace_manager_v1_add_listener(wayland->workspace_manager, &workspace_manager_listener, this);
+  // wl_display_roundtrip(wayland->display);
+}
+
+void Workspace::poll_events() {
+  auto wayland = Wayland::get_singleton();
+
+  if (!wayland->display) {
+    return;
+  }
+
+  while (wl_display_prepare_read(wayland->display) != 0) {
+    wl_display_dispatch_pending(wayland->display);
+  }
+
+  wl_display_flush(wayland->display);
+
+  int fd = wl_display_get_fd(wayland->display);
+  struct pollfd pfd = {.fd = fd, .events = POLLIN, .revents = 0};
+  int r = poll(&pfd, 1, 0);
+
+  if (r > 0 && (pfd.revents & POLLIN)) {
+    wl_display_read_events(wayland->display);
+    wl_display_dispatch_pending(wayland->display);
+  } else {
+    wl_display_cancel_read(wayland->display);
+  }
 }
