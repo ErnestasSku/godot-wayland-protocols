d7c743a5c5af9c4c3ebebf7a9f66ff7cedccda2b
ernestassku <ernestassku@gmail.com>
2025-12-29 10:36:54 +0200

    Add variout events to workspace view clas

--- src/workspace/workspace_view_gd.cpp
+++ src/workspace/workspace_view_gd.cpp
@@ -1,14 +1,15 @@
 #include "workspace/workspace_view_gd.h"
-
 #include "godot_cpp/classes/global_constants.hpp"
 #include "godot_cpp/core/binder_common.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/object.hpp"
+#include "godot_cpp/core/property_info.hpp"
 #include "workspace/workspace.h"
 #include "workspace/workspace_manager.h"
 #include <cstdint>
 #include <wayland-util.h>
 
+// TODO: implement workspace_capabilities enum later.
 VARIANT_ENUM_CAST(WorkspaceViewGD::StateFlags);
 
 void WorkspaceViewGD::_bind_methods() {
@@ -25,12 +26,40 @@ void WorkspaceViewGD::_bind_methods() {
   BIND_ENUM_CONSTANT(ACTIVE);
   BIND_ENUM_CONSTANT(URGENT);
   BIND_ENUM_CONSTANT(HIDDEN);
+
+  ADD_SIGNAL(MethodInfo("id_changed", PropertyInfo(Variant::STRING, "id")));
+  ADD_SIGNAL(MethodInfo("name_changed", PropertyInfo(Variant::STRING, "name")));
+  ADD_SIGNAL(MethodInfo("coordinates_changed", PropertyInfo(Variant::PACKED_INT32_ARRAY, "coordinates")));
+  ADD_SIGNAL(MethodInfo("workspace_state_changed",
+                        PropertyInfo(Variant::INT, "state", PROPERTY_HINT_FLAGS, "WorkspaceState")));
+  ADD_SIGNAL(MethodInfo("capabilities_changed", PropertyInfo(Variant::INT, "capabilities")));
+  ADD_SIGNAL(MethodInfo("workspace_removed"));
 }
 
 void WorkspaceViewGD::_init_view(std::shared_ptr<WorkspaceManager> manager, Workspace *workspace, uint64_t runtime_id) {
   m_manager = std::move(manager);
   m_workspace = workspace;
   m_runtime_id = runtime_id;
+
+  workspace->on_id_changed([this](std::string id) { call_deferred("emit_signal", "id_changed", id.c_str()); });
+  workspace->on_name_changed([this](std::string name) { call_deferred("emit_signal", "name_changed", name.c_str()); });
+  workspace->on_coordinates_changed([this](wl_array *coordinates) {
+    PackedInt32Array arr;
+    const size_t count = coordinates->size / sizeof(uint32_t);
+    arr.resize(static_cast<int>(count));
+
+    const uint32_t *data = static_cast<const uint32_t *>(coordinates->data);
+    for (size_t i = 0; i < count; i++) {
+      arr.set(static_cast<int>(i), static_cast<int32_t>(data[i]));
+    }
+
+    call_deferred("emit_signal", "coordinates_changed", arr);
+  });
+  workspace->on_state_changed(
+      [this](uint32_t state) { call_deferred("emit_signal", "workspace_state_changed", state); });
+  workspace->on_capabilities_changed(
+      [this](uint32_t capabilities) { call_deferred("emit_signal", "capabilities_changed", capabilities); });
+  workspace->on_removed([this]() { call_deferred("emit_signal", "workspace_removed"); });
 }
 
 int64_t WorkspaceViewGD::get_runtime_id() const { return static_cast<int64_t>(m_runtime_id); }
@@ -51,7 +80,6 @@ String WorkspaceViewGD::get_name() const {
   return String(m_workspace->name().c_str());
 }
 
-// TODO: consider implementing a WorkspaceState godot facint enum/class
 int64_t WorkspaceViewGD::get_state() const {
   if (!m_workspace) {
     return 0;
